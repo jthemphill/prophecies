1216dcf62b1797dbb7783ce6584a0e5357b273ac
Jeff Hemphill <jhemphill@fb.com>
2020-07-26 13:36:11 -0700

    Updates

--- bootstrap.js
+++ bootstrap.js
@@ -198,7 +198,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/prophecies_bg.wasm":"8422f42095561405fb44"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/prophecies_bg.wasm":"7c76cd5c642ddb4dccab"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
