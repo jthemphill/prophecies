039fc7edd420f929cb18d6c815a08c99b6539df2
Jeff Hemphill <jhemphill@fb.com>
2020-07-25 23:11:44 -0700

    Updates

---
+++ index.js
@@ -0,0 +1,235 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = __importDefault(require("react"));
+const react_dom_1 = __importDefault(require("react-dom"));
+const wasm = __importStar(require("prophecies"));
+const CELL_SIZE_PX = 100;
+const GRID_COLOR = "#999999";
+const HUMAN_PLAYER = 0;
+const BOT_PLAYER = 1;
+const HUMAN_COLOR = "#d63b3b";
+const BOT_COLOR = "#3bd6d0";
+const MIN_PLAYOUTS = 2048;
+const PONDER_MS = 128;
+const PONDER_INTERVAL = 1024;
+const legalActionRegex = new RegExp("^X|[0-9]+$");
+class Cell extends react_1.default.PureComponent {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            content: "",
+        };
+    }
+    render() {
+        return react_1.default.createElement("td", null,
+            react_1.default.createElement("input", { disabled: this.props.guess !== null, inputMode: "numeric", onBlur: this.onBlur.bind(this), onChange: this.onChange.bind(this), style: {
+                    color: this.getColor(),
+                }, value: this.getContent() }));
+    }
+    getColor() {
+        if (this.props.player == HUMAN_PLAYER || this.state.content.length > 0) {
+            return HUMAN_COLOR;
+        }
+        else if (this.props.player == BOT_PLAYER) {
+            return BOT_COLOR;
+        }
+        return null;
+    }
+    getContent() {
+        if (this.props.guess == null) {
+            return this.state.content;
+        }
+        if (this.props.guess === 0) {
+            return "X";
+        }
+        return this.props.guess;
+    }
+    onBlur(e) {
+        if (legalActionRegex.test(this.state.content)) {
+            try {
+                this.props.onBlur(this.props.row, this.props.col, e);
+            }
+            catch (err) {
+                console.log(err);
+            }
+        }
+        this.setState({ content: "" });
+    }
+    onChange(e) {
+        this.setState({ content: e.currentTarget.value });
+        return this.props.onChange(this.props.row, this.props.col, e);
+    }
+}
+class Grid extends react_1.default.PureComponent {
+    constructor(props, context) {
+        super(props, context);
+        this.state = this.getGameState();
+    }
+    componentDidMount() {
+        this.bot = wasm.WasmBot.new(this.props.nrows, this.props.ncols, HUMAN_PLAYER);
+        this.setState(this.getGameState());
+        this.shouldPonder = true;
+        this.ponder();
+    }
+    componentWillUnmount() {
+        this.shouldPonder = false;
+        this.bot.free();
+    }
+    getGameState() {
+        let grid = [];
+        for (let r = 0; r < this.props.nrows; ++r) {
+            let row = [];
+            for (let c = 0; c < this.props.ncols; ++c) {
+                if (this.bot) {
+                    const cell = this.bot.get_cell(r, c);
+                    row.push({
+                        guess: cell.guess !== undefined ? cell.guess : null,
+                        player: cell.player !== undefined ? cell.player : null,
+                    });
+                }
+                else {
+                    row.push({
+                        guess: null,
+                        player: null,
+                    });
+                }
+            }
+            grid.push(row);
+        }
+        return {
+            activePlayer: this.bot ? this.bot.get_active_player() : HUMAN_PLAYER,
+            winProb: this.getWinProb(),
+            scores: this.bot ? [...this.bot.get_scores()] : [0, 0],
+            grid,
+        };
+    }
+    render() {
+        if (this.state.activePlayer === BOT_PLAYER) {
+            window.setTimeout(this.takeBotAction.bind(this), 0);
+        }
+        const rows = [];
+        for (let r = 0; r < this.props.nrows; r++) {
+            const row = [];
+            for (let c = 0; c < this.props.ncols; c++) {
+                const { guess, player } = this.state.grid[r][c];
+                row.push(react_1.default.createElement(Cell, { col: c, guess: guess, key: `${r},${c}`, onBlur: this.onCellInputBlur.bind(this), onChange: this.onCellInputChange.bind(this), player: player, row: r }));
+            }
+            rows.push(react_1.default.createElement("tr", { key: r }, row));
+        }
+        let cpuProb = null;
+        if (this.state.winProb != null) {
+            cpuProb = react_1.default.createElement("div", { className: "prediction" },
+                "The CPU thinks you have a ",
+                react_1.default.createElement("span", { style: { color: HUMAN_COLOR } }, formatPercent(this.state.winProb)),
+                " chance of winning.");
+        }
+        return react_1.default.createElement("div", { className: "prophecies-game" },
+            react_1.default.createElement("table", null,
+                react_1.default.createElement("tbody", null, rows)),
+            react_1.default.createElement("div", { className: "scores" },
+                react_1.default.createElement("div", null, "Scores:"),
+                react_1.default.createElement("div", null,
+                    react_1.default.createElement("span", { style: { color: HUMAN_COLOR } }, this.state.scores[0]),
+                    react_1.default.createElement("span", null, " - "),
+                    react_1.default.createElement("span", { style: { color: BOT_COLOR } }, this.state.scores[1])),
+                cpuProb));
+    }
+    onCellInputBlur(row, col, e) {
+        // Already made a move in this cell
+        if (this.state.grid[row][col].player !== null) {
+            return;
+        }
+        const cellValue = e.currentTarget.value;
+        if (!cellValue || !cellValue.length || !legalActionRegex.test(cellValue)) {
+            return;
+        }
+        const guess = cellValue === "X" ? 0 : parseInt(cellValue, 10);
+        this.takeAction({ row, col, guess });
+    }
+    onCellInputChange(row, col, e) {
+        const target = e.target;
+        if (!legalActionRegex.test(target.value)) {
+            console.log(`${target.value} failed regex`);
+            return;
+        }
+    }
+    ponder() {
+        if (!this.shouldPonder) {
+            return;
+        }
+        if (this.bot.is_finished()) {
+            this.shouldPonder = false;
+            return;
+        }
+        const t0 = performance.now();
+        let nplayouts = 0;
+        while (performance.now() - t0 < PONDER_MS) {
+            this.bot.playout();
+            ++nplayouts;
+        }
+        const tf = performance.now();
+        console.log(`${nplayouts} playouts in ${tf - t0} ms`);
+        this.setState({ winProb: this.getWinProb() });
+        window.setTimeout(this.ponder.bind(this), PONDER_INTERVAL);
+    }
+    getWinProb() {
+        if (!this.bot) {
+            return null;
+        }
+        if (this.bot.get_active_player() !== HUMAN_PLAYER) {
+            return null;
+        }
+        const edge = this.bot.get_best_action();
+        if (edge == null) {
+            return null;
+        }
+        return (1 + edge.score / Number(edge.visits)) / 2;
+    }
+    takeAction(action) {
+        if (!action) {
+            return;
+        }
+        // Throws IllegalMoveError if move is illegal
+        this.bot.place(action.row, action.col, action.guess);
+        this.setState(this.getGameState());
+    }
+    takeBotAction() {
+        for (let i = 0; i < MIN_PLAYOUTS; ++i) {
+            this.bot.playout();
+        }
+        const edge = this.bot.get_best_action();
+        if (edge == null) {
+            this.shouldPonder = false;
+            return;
+        }
+        this.takeAction(edge.action);
+    }
+}
+function formatPercent(fraction) {
+    const rounded = new Intl.NumberFormat('en-US', { maximumSignificantDigits: 4 }).format(fraction * 100);
+    return `${rounded}%`;
+}
+react_dom_1.default.render(react_1.default.createElement(Grid, { nrows: 4, ncols: 4 }), document.getElementById("prophecies-grid-container"));
+//# sourceMappingURL=index.js.map
